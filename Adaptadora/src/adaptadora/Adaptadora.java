06a7151ee41c375cd357fd2ee1dc68199b6c99dc
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-06 13:24:40 -0300

    add adaptadora project

---
+++ Adaptadora/src/adaptadora/Adaptadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adaptadora;
+
+/**
+ *
+ * @author Giovani Paganini <giovanipaganini@outlook.com>
+ */
+public class Adaptadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        
+        sout
+    }
+    
+}

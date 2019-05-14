8893344ccb2e005782bc59c823a7e45e2e42b82d
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-14 14:24:15 -0300

    added subtracao method for subtraction

--- Adaptadora/src/com/fabricadescripts/Calculadora.java
+++ Adaptadora/src/com/fabricadescripts/Calculadora.java
@@ -17,10 +17,21 @@ public class Calculadora {
         res = num1 + num2;
 
         System.out.println(num1 + " + " + num2 + " = " + res + "\n");
-
         return res;
     }
 
-    
+    public int subtracao() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: \n");
+        num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: \n");
+        num2 = scanner.nextInt();
+
+        res = num1 - num2;
+
+        System.out.println(num1 + " - " + num2 + " = " + res + "\n");
+        return res;
+    }
 
 }

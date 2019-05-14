78ba0d5839a25eb84c4598734ca1dd93eb94dac9
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-14 14:43:11 -0300

    division and multiplication implementation

--- Adaptadora/src/com/fabricadescripts/Calculadora.java
+++ Adaptadora/src/com/fabricadescripts/Calculadora.java
@@ -34,4 +34,32 @@ public class Calculadora {
         return res;
     }
 
+    public int multiplicacao() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: \n");
+        num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: \n");
+        num2 = scanner.nextInt();
+
+        res = num1 * num2;
+
+        System.out.println(num1 + " x " + num2 + " = " + res + "\n");
+        return res;
+    }
+
+    public int divisao() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: \n");
+        num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: \n");
+        num2 = scanner.nextInt();
+
+        res = num1 / num2;
+
+        System.out.println(num1 + " / " + num2 + " = " + res + "\n");
+        return res;
+    }
+
 }

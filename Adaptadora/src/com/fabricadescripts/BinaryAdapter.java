166596940ef5c73c9da4e0d34d06beed36561937
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-14 14:28:15 -0300

    implementation of binary adapter

--- Adaptadora/src/com/fabricadescripts/BinaryAdapter.java
+++ Adaptadora/src/com/fabricadescripts/BinaryAdapter.java
@@ -1,4 +1,8 @@
 package com.fabricadescripts;
 
-public class BinaryAdapter {
+public class BinaryAdapter extends Calculadora {
+
+    public void Dec2Bin(Integer res) {
+        System.out.println("Binário: " + Integer.toString(res, 2));
+    }
 }

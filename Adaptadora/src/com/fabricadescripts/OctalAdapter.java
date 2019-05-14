913fe4de62577c30e8bf9ef732a77cc8778fca1d
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-14 15:24:18 -0300

    Created Octal adapter

---
+++ Adaptadora/src/com/fabricadescripts/OctalAdapter.java
@@ -0,0 +1,8 @@
+package com.fabricadescripts;
+
+public class OctalAdapter extends Calculadora {
+
+    public void Dec2Octa(Integer res) {
+        System.out.println("Octal: " + Integer.toString(res, 8));
+    }
+}

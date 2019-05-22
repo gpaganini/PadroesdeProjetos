d8d6e51e4ce3e297f61c38a00ecf8ac1ba12b332
Giovani Paganini <giovanipaganini@outlook.com>
2019-05-22 11:16:20 -0300

    teste

--- DonPipposMacarroni/src/com/donpippos/Main.java
+++ DonPipposMacarroni/src/com/donpippos/Main.java
@@ -1,22 +1,45 @@
 package com.donpippos;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-	    Prato meuPrato = new Spaghetti();
+        int res = 0;
+        String restart;
 
-        System.out.println(meuPrato.getNome() + " = "
-                + meuPrato.getPreco());
+        Scanner io = new Scanner(System.in);
+        System.out.println("Escolha a operação " +
+                "\n 1 [+] " +
+                "\n 2 [-] " +
+                "\n 3 [*] " +
+                "\n 4 [/] " +
+                "\n Por favor digite o número da operação\n");
 
-        meuPrato = new Bacon(meuPrato);
-        System.out.println(meuPrato.getNome() + " = "
-                + meuPrato.getPreco());
+        int escolha = io.nextInt();
 
-        meuPrato = new Calabresa(meuPrato);
-        System.out.println(meuPrato.getNome() + " = "
-                + meuPrato.getPreco());
+        if (escolha == 1) {
+            Prato meuPrato = new Spaghetti();
+            System.out.println(meuPrato.getNome() + " = "
+                    + meuPrato.getPreco());
 
-        meuPrato = new Mozzarella(meuPrato);
-        System.out.println(meuPrato.getNome() + " = "
-                + meuPrato.getPreco());
+            Scanner ingr = new Scanner(System.in);
+            System.out.println("ingredientes: ");
+
+            int ing = ingr.nextInt();
+
+            if (ing == 1) {
+                meuPrato = new Bacon(meuPrato);
+                System.out.println(meuPrato.getNome() + " = "
+                        + meuPrato.getPreco());
+                meuPrato = new Calabresa(meuPrato);
+                System.out.println(meuPrato.getNome() + " = "
+                        + meuPrato.getPreco());
+
+                meuPrato = new Mozzarella(meuPrato);
+                System.out.println(meuPrato.getNome() + " = "
+                        + meuPrato.getPreco());
+            }
+
+        }
     }
 }
